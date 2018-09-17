39a75efdbe4a0f22cbe383fff31318d4c6cf98cb
Oleg Nykolyn <juravel2@gmail.com>
2018-09-17 15:54:58 +0300

    Add packaging files.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='django-object-manager',
+    version='0.0.1',
+    description='Django object manager for tests',
+    license='BSD',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/K0Te/django-object-manager',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django tests',
+    install_requires=['django'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-cov', 'pytest-django'],
+    test_suite='tests.functional',
+    packages=find_packages(exclude=['tests']),  # Required
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/K0Te/django-object-manager/issues',
+        'Source': 'https://github.com/K0Te/django-object-manager',
+    },
+)
