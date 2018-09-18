c2ab015763545937775541c8e992d26c9974e634
Oleg Nykolyn <juravel2@gmail.com>
2018-09-18 16:31:11 +0300

    Python 3.6+ only.

--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@ setup(
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     keywords='django tests',
