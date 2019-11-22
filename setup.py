2afffff930f403f442228ab84bc87caab91b5195
Oleg Nykolyn <juravel2@gmail.com>
2019-11-22 16:30:15 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-object-manager',
-    version='0.0.7',
+    version='0.0.8',
     description='Django object manager for tests',
     license='BSD',
     long_description=long_description,
